ad6d044b3098fdf7dd8c46a7f123c2644b537c48
Ming <vincentmliu@hotmail.com>
2018-07-23 16:31:44 +0800

    0026类

---
+++ src/com/xixi/easy/id0026/Solotion.java
@@ -0,0 +1,18 @@
+package com.xixi.easy.id0026;
+
+public class Solotion {
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
