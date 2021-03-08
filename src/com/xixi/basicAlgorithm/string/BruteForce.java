620a54e8e8c0f050eb2a370b3441f051540b2f30
liuming <vincentmliu@hotmail.com>
2021-03-08 17:08:15 +0800

    提交测试，bf算法

---
+++ src/com/xixi/basicAlgorithm/string/BruteForce.java
@@ -0,0 +1,51 @@
+package com.xixi.basicAlgorithm.string;
+
+public class BruteForce {
+
+
+    public static int bruteForce(String str, String pattern) {
+        int resultIndex= -1;
+
+        //校验
+        if(str == null || pattern == null) return resultIndex;
+        if(str.isEmpty()|| pattern.isEmpty()) return resultIndex;
+
+        char[] strChars = str.toCharArray();
+        char[] patternChars = pattern.toCharArray();
+
+        int strCharsLen = strChars.length;
+        int patternCharsLen = patternChars.length;
+
+        int start = 0;
+
+        while(start <= (strCharsLen-patternCharsLen)){
+            int i = 0;
+            while(i < patternCharsLen){
+                if(strChars[start + i] != patternChars[i]){
+                    break;
+                }
+                i++;
+            }
+            if(i == patternCharsLen){
+                return start;
+            }
+
+            start++;
+        }
+
+
+
+
+        return resultIndex;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "ATGTGAGCTGGTGTGTGCFAA";
+        String pattern = "GCFAA";
+        int index = bruteForce(str, pattern);
+        System.out.println("首次出现位置：" + index);
+    }
+
+
+}
